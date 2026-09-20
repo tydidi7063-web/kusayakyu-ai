29cbacd07f6320ef1474009d45372e8a10f2b81e
tydidi7063-web <tydidi7063@ezweb.ne.jp>
2026-09-20 19:16:44 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE="kusayakyu-ai-v49";
+const CACHE="kusayakyu-ai-v51";
 const CORE=["./","./index.html","./viewer.html","./manifest.webmanifest","./icon-192.png","./icon-512.png"];
 self.addEventListener("install",e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(CORE)).then(()=>self.skipWaiting())));
 self.addEventListener("activate",e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
